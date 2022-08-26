9aed56ac56cb400aa5e815d46ee9ddf1142f88ed
Saddam-tech <salokhiddinov0727@gmail.com>
2022-08-26 23:27:20 +0900

    added a main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import { router } from "./components/router";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBox className="appBox">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;500;700;900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap"
+        rel="stylesheet"
+      />
+
+      <HashRouter>
+        <Routes>
+          {router.map((el, _) => (
+            <Route path={el.path} element={el.element} />
+          ))}
+        </Routes>
+      </HashRouter>
+    </AppBox>
   );
 }
 
-export default App;
+const AppBox = styled.div`
+  display: flex;
+`;
---
+++ src/components/router.js
@@ -0,0 +1,9 @@
+import Main from "./Main";
+
+export const router = [
+  {
+    route: "Main",
+    path: "/",
+    element: <Main />,
+  },
+];
