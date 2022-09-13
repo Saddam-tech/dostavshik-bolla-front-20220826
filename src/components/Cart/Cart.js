84d83149d4f8e5dd85970dc4c2fcbce3312e5540
Saddam-tech <salokhiddinov0727@gmail.com>
2022-09-13 17:46:09 +0900

    latest changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import VerifiedIcon from "@mui/icons-material/Verified";
 
 export default function Cart() {
   const navigate = useNavigate();
@@ -17,6 +18,34 @@ export default function Cart() {
         </div>
         <span className="edit">Ozgartirmoq</span>
       </div>
+      <div className="section-2">
+        <h3>Milliy Taomlar</h3>
+        <VerifiedIcon sx={{ color: "#f99500" }} />
+        <p className="highlight">Yaguar eltuv</p>
+      </div>
+      <div className="section-3">
+        <div className="sub-section-1">
+          <p>1. Palov </p>
+          <CloseOutlinedIcon sx={{ color: "#c1bdbd" }} />
+        </div>
+        <div className="sub-section-1">
+          <p>6,900 UZS</p>
+          <p>[dropdown]</p>
+        </div>
+      </div>
+      <div className="section-3">
+        <p className="add">+ Savatchaga qo'shmoq</p>
+      </div>
+      <div className="section-3">
+        <div className="sub-section-1">
+          <p>Umumiy</p>
+          <p>6,900 UZS</p>
+        </div>
+        <div className="sub-section-1">
+          <p>Yetkazib berish</p>
+          <p>+3,000 UZS</p>
+        </div>
+      </div>
     </Container>
   );
 }
@@ -32,7 +61,7 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-start;
-    gap: 130px;
+    gap: 140px;
     width: 100%;
     padding: 20px;
 
@@ -44,14 +73,14 @@ const Container = styled.div`
   .section-1 {
     display: flex;
     font-size: 12px;
-    border-bottom: 5px solid #efefef;
+    border-bottom: 11px solid #f4f4f4;
     width: 100%;
 
     .inner-section-1 {
       display: flex;
       flex-direction: column;
       width: 100%;
-      padding: 10px;
+      padding: 20px;
     }
 
     .bold {
@@ -61,8 +90,53 @@ const Container = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
-      padding: 10px;
-      color: #6bd7ff;
+      padding: 20px;
+      color: #21c0ff;
+    }
+  }
+
+  .section-2 {
+    display: flex;
+    align-items: center;
+    flex-direction: flex-start;
+    width: 100%;
+    gap: 7px;
+    font-size: 13px;
+    padding: 20px;
+    border-bottom: 3px solid #f4f4f4;
+
+    .highlight {
+      color: #f99500;
+      font-weight: 600;
+      font-size: 14px;
+    }
+  }
+
+  .section-3 {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+
+    .add {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      text-align: center;
+      border-top: 3px solid #f4f4f4;
+      border-bottom: 7px solid #f4f4f4;
+      height: 90px;
+      color: #21c0ff;
+      font-weight: 500;
+    }
+
+    .sub-section-1 {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 20px;
+      width: 100%;
     }
   }
 `;
