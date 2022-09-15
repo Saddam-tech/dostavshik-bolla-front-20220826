015de15966d23d4bd339cdfd866a20d08365141c
Saddam-tech <salokhiddinov0727@gmail.com>
2022-09-15 21:32:14 +0900

    finished publishing myprofile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 import React, { useState } from "react";
 import Carousel from "../Carousel/Carousel";
-
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { useNavigate } from "react-router-dom";
-import { navigationitems } from "../helper";
+import { navigationitems, profileOpt } from "../helper";
 export default function Profile() {
   const navigate = useNavigate();
+  const [value, setValue] = React.useState(0);
 
   return (
     <Container>
@@ -29,6 +37,58 @@ export default function Profile() {
         <button>Koproq ko'rmoq</button>
       </section>
       <Carousel height={22} padding="0" objectFit="cover" />
+      <section className="section-1">
+        <ul>
+          {profileOpt.map((el, i) => (
+            <li key={i}>
+              <span>{el.icon}</span>
+              <span>{el.label}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+      {/* New restaurants section*/}
+      <Paper
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            paddingTop: "8px",
+            height: 85,
+          }}
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Asosiy" icon={<HomeOutlinedIcon />} />
+          <BottomNavigationAction
+            label="Qidirish"
+            icon={<SearchOutlinedIcon />}
+          />
+          <BottomNavigationAction
+            label="Sevimli"
+            icon={<FavoriteBorderOutlinedIcon />}
+          />
+          <BottomNavigationAction
+            label="Buyurtmalar"
+            icon={<AssignmentOutlinedIcon />}
+          />
+          <BottomNavigationAction
+            label="Profil"
+            icon={<PersonOutlineOutlinedIcon />}
+          />
+        </BottomNavigation>
+      </Paper>
     </Container>
   );
 }
@@ -75,6 +135,30 @@ const Container = styled.div`
       border-radius: 5px;
       color: #21c0ff;
       font-weight: 600;
+    }
+  }
+
+  .section-1 {
+    width: 100%;
+    padding: 15px 15px 90px 15px;
+
+    ul {
+      display: flex;
+      width: 100%;
+      height: 100%;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+
+      li {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 10px;
+        gap: 10px;
+        font-size: 18px;
+        width: 100%;
+      }
     }
   }
 `;
--- src/components/helper.js
+++ src/components/helper.js
@@ -1,3 +1,14 @@
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SellOutlinedIcon from "@mui/icons-material/SellOutlined";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import PaymentIcon from "@mui/icons-material/Payment";
+import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
+import LiveHelpOutlinedIcon from "@mui/icons-material/LiveHelpOutlined";
+import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import CampaignOutlinedIcon from "@mui/icons-material/CampaignOutlined";
+import FeaturedPlayListOutlinedIcon from "@mui/icons-material/FeaturedPlayListOutlined";
 export const navigationitems = [
   "Taniqli",
   "Menyu",
@@ -11,4 +22,53 @@ export const options = [
   { label: "Q'oy go'shti qo'shmoq", price: "(+ 3,000 UZS)" },
   { label: "Tovuq go'shti qo'shmoq", price: "(+ 2,500 UZS)" },
   { label: "Mol go'shti qo'shmoq", price: "(+ 4,300 UZS)" },
+];
+
+export const profileOpt = [
+  {
+    label: "Manzillar",
+    icon: <ListAltIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Mening sevimli",
+    icon: <FavoriteBorderIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Promotsion kod",
+    icon: <SellOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Tadbirlar",
+    icon: <EventAvailableIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Tolov",
+    icon: <PaymentIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Ovqat yetkazish",
+    icon: <DeliveryDiningIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "FAQ",
+    icon: <LiveHelpOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Biz bilan bog'lanish",
+    icon: <CallOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Sozlamalar",
+    icon: <SettingsOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "E'lonlar",
+    icon: <CampaignOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />,
+  },
+  {
+    label: "Foydalanish shartlari",
+    icon: (
+      <FeaturedPlayListOutlinedIcon sx={{ fontSize: 35, color: "#595959" }} />
+    ),
+  },
 ];
