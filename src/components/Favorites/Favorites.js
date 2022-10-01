c4cee825e350425a1f06aa3a1d5e901f7ebd499e
Saddam-tech <salokhiddinov0727@gmail.com>
2022-10-02 02:41:28 +0900

    favorites page has been completed

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from "react";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useNavigate, useParams } from "react-router-dom";
+import Post from "./Post";
 
 export default function Favorites() {
   const [value, setValue] = useState(3);
   const { uuid } = useParams();
   const navigate = useNavigate();
+  const [posts, setPosts] = useState(Array.from(new Array(10)));
 
   return (
     <Container>
@@ -18,14 +20,19 @@ export default function Favorites() {
           <p className="edit">O'zgartirmoq</p>
         </div>
         <div className="inner_bar_1">
-          <p className="settings">1 dona</p>
+          <p className="settings">10 dona</p>
           <p className="settings">
-            Doim buyurtma{" "}
+            <p className="filter"> Doim buyurtma</p>{" "}
             <span>
-              <KeyboardArrowDownIcon />
+              <KeyboardArrowDownIcon sx={{ color: "#919191" }} />
             </span>
           </p>
         </div>
+      </section>
+      <section className="posts">
+        {posts.map((el, i) => (
+          <Post />
+        ))}
       </section>
     </Container>
   );
@@ -37,14 +44,18 @@ const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   width: 100%;
-  margin-top: 30px;
+  margin: 130px 0 20px 0;
   .navbar {
     display: flex;
     gap: 10px;
     flex-direction: column;
     width: 100%;
     border-bottom: 11px solid #f4f4f4;
-    padding-bottom: 10px;
+    padding: 30px 0 10px 0;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    height: 150px;
 
     .inner_bar_1 {
       padding-left: 30px;
@@ -73,9 +84,22 @@ const Container = styled.div`
         justify-content: center;
         gap: 5px;
         span {
+          display: flex;
+          align-items: center;
+          justify-content: center;
           padding-right: 10px;
+        }
+        .filter {
+          color: #919191;
+          font-size: 14px;
         }
       }
     }
+  }
+  .posts {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
 `;
---
+++ src/components/Favorites/Post.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useNavigate, useParams } from "react-router-dom";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
+
+export default function Post() {
+  const [value, setValue] = useState(3);
+  const { uuid } = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <img
+        src="https://vegnews.com/media/W1siZiIsIjI5NDQ2L1ZlZ05ld3MuVmVnYW5GYXN0Rm9vZC5Nb250eXNHb29kQnVyZ2VyLmpwZyJdLFsicCIsInRodW1iIiwiMTYwMHg5NDYjIix7ImZvcm1hdCI6ImpwZyJ9XSxbInAiLCJvcHRpbWl6ZSJdXQ/VegNews.VeganFastFood.MontysGoodBurger.jpg?sha=892e9c726614c0f8"
+        alt=""
+      />
+      <div className="sub-container">
+        <div className="inner-div">
+          <h4>Les Ailes</h4>
+          <div className="delivery">
+            <VerifiedIcon sx={{ color: "#f99500", fontSize: 18 }} />
+            <p className="highlight">Yaguar eltuv</p>
+          </div>
+        </div>
+        <div className="inner-div-1">
+          <StarOutlinedIcon sx={{ color: "#fff600", fontSize: 18 }} />
+          <p>4.9(386)</p>
+        </div>
+        <div className="inner-div-1">
+          <p>4.9(386) * 1.4km ·</p>
+          <p>14 ~ 24 min ·</p>
+          <p className="bold">Tekin Eltuv ~</p>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 100%;
+  margin-top: 30px;
+  img {
+    width: 120px;
+    height: 120px;
+    object-fit: cover;
+  }
+  .sub-container {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    gap: 4px;
+    .inner-div {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding-left: 14px;
+      h4 {
+        width: 100%;
+        font-weight: 500;
+      }
+
+      .delivery {
+        display: flex;
+        width: 100%;
+        gap: 4px;
+        color: #3a3a3a;
+        .highlight {
+          color: #f99500;
+          font-weight: 600;
+          font-size: 12px;
+          width: 100%;
+          white-space: nowrap;
+        }
+      }
+    }
+    .inner-div-1 {
+      display: flex;
+      align-items: center;
+      padding-left: 14px;
+      p {
+        font-size: 10px;
+      }
+      .bold {
+        font-weight: 700;
+        color: #919191;
+      }
+    }
+  }
+`;
